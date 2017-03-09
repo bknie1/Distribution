7bfe962781d69f6bdbb61ad1fdc7fe2102dd38f4
bknie1 <brandonknieriem26@gmail.com>
2017-03-09 17:00:27 -0500

    Tally all alpha chars.

--- LetterDist.py
+++ LetterDist.py
@@ -11,6 +11,7 @@ import curses.ascii
 #DECLARATIONS####################################################################
 
 total_letters = 0
+occurrences = [0] * 26
 
 #FX###############################################################################
 # Iterates through the book, filtering white space, and tallies each ASCII val.
@@ -18,20 +19,30 @@ def Read_File(file_name) :
 	with open(file_name) as file :
 		for line in file : 						# Reads line.
 			for letter in line : 				# Reads character value from line.
-				if not letter.isspace() : 		# Filters white space.
-					letter = letter.lower()		# Sanitization.
-					#if ord(letter)
-					Tally_Value(letter)			# Hit! Process it.
+				Sanitize_Input(letter)
 #--------------------------------------------------------------------------------#
-def Sanitize_Input(value) :
-	return
+def Sanitize_Input(ch) :
+	if not ch.isspace() : 						# Filters white space.
+		ch = ch.lower()							# Sanitizes for range.
+		if ord(ch) >= 97 and ord(ch) <= 123 : 	# Within lower alpha range.
+			Tally_Value(ch)						# Hit! Process it.
 #--------------------------------------------------------------------------------#
 # Add increment array slot via ASCII index (left adjusted).
 # Ex. a = 97. array[0] = a's ascii value - 97 constant.
-def Tally_Value(char) :
-	if ord(char) > 160 : print(char)
+def Tally_Value(ch) :
 	global total_letters
+	global occurrences
 	total_letters += 1
+	target = ord(ch) - 97 # Left adjustment: 0/a - 25/z.
+	occurrences[target] += 1
+#--------------------------------------------------------------------------------#
+def Print_Result() :
+	global occurrences
+	print("Total number of letters:", total_letters)
+	k = 97
+	for i in occurrences :
+		print(chr(k),'=', i)
+		k += 1
 #--------------------------------------------------------------------------------#
 def Print_Wiki() :
 	print("Wiki Letter Frequency:")
@@ -39,12 +50,8 @@ def Print_Wiki() :
 	while i < 123 :
 		print(chr(i), "=")
 		i += 1
-
 #--------------------------------------------------------------------------------#
-def Print_Result() :
-	print("Total number of letters:", total_letters)
-#--------------------------------------------------------------------------------#
-# Error handling template.
+# Error handling.
 def Throw_Fatal(error_text) :
 	print("Error:", error_text)
 	sys.exit(1)
