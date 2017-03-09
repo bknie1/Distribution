49b398ea3d185e2ab0e8b61e45f60b84e12ac493
bknie1 <brandonknieriem26@gmail.com>
2017-03-09 15:42:52 -0500

    Read file, read line, read each value. Count letters.

---
+++ LetterDist.py
@@ -0,0 +1,59 @@
+# Name: 		Assignment 3 : Letter Distribution
+# Description : Counts letter occurrences in argument input text file.
+# Author:		Brandon Knieriem
+
+#LIB##############################################################################
+
+import sys
+import os
+import curses.ascii
+
+#DECLARATIONS####################################################################
+
+total_letters = 0
+
+#FX###############################################################################
+# Iterates through the book, filtering white space, and tallies each ASCII val.
+def Read_File(file_name) :
+	with open(file_name) as file :
+		for line in file : 						# Reads line.
+			for letter in line : 				# Reads character value from line.
+				if not letter.isspace() : 		# Filters white space.
+					letter = letter.lower()
+					Tally_Value(letter)			# Hit! Process it.
+#--------------------------------------------------------------------------------#
+def Tally_Value(char) :
+	global total_letters
+	total_letters += 1
+#--------------------------------------------------------------------------------#
+def Print_Wiki() :
+	print("Wiki Letter Frequency:")
+	i = 97
+	while i < 123 :
+		print(chr(i), "=")
+		i += 1
+
+#--------------------------------------------------------------------------------#
+def Print_Result() :
+	print("Total number of letters:", total_letters)
+#--------------------------------------------------------------------------------#
+
+def Throw_Fatal(error_text) :
+	print("Error:", error_text)
+	sys.exit(1)
+
+#MAIN#############################################################################
+
+# Argument Filtering
+arg_num = len(sys.argv)
+if arg_num != 2 : Throw_Fatal("Invalid arguments.")
+file = sys.argv[1]
+if os.path.isfile(file) == False or file.endswith(".txt") == False :
+	Throw_Fatal("Invalid file.")
+
+# File Processing
+Read_File(file)
+Print_Result()
+
+#Print_Wiki()
+#print("Total Letter Count:", total_letters)
