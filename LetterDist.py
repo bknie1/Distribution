c9f0dbd5d4253957b07c8d27ca86cbe65699b4c2
bknie1 <brandonknieriem26@gmail.com>
2017-03-10 20:24:48 -0500

    Compare to Wiki stats.

--- LetterDist.py
+++ LetterDist.py
@@ -10,12 +10,20 @@ import curses.ascii
 
 #DECLARATIONS####################################################################
 
+wiki_file = "official.txt" # For easy file swapping.
 total_letters = 0
 occurrences = [0] * 26
+stats = []
 
 #FX###############################################################################
+def Read_Wiki_File() :
+	global stats
+	with open(wiki_file) as file :
+		for line in file :
+			stats.append(line.rstrip())
+#--------------------------------------------------------------------------------#
 # Iterates through the book, filtering white space, and tallies each ASCII val.
-def Read_File(file_name) :
+def Read_Arg_File(file_name) :
 	with open(file_name) as file :
 		for line in file : 						# Reads line.
 			for letter in line : 				# Reads character value from line.
@@ -38,10 +46,23 @@ def Tally_Value(ch) :
 #--------------------------------------------------------------------------------#
 def Print_Result() :
 	global occurrences
+	global stats
 	print("Total number of letters:", total_letters)
 	k = 97
+	p = 1
+	mod = 1 # Adjust for column formatting.
 	for i in occurrences :
-		print(chr(k),'=', i)
+		letter = chr(k)
+		text_avg = round((i/total_letters * 100), 2)
+		global_avg = float(stats[k - 97])
+		difference = text_avg - global_avg
+		print(letter, sep='', end=' = ')
+		print("%10d" % i, sep='', end='\t')
+		print("Average - Text: %4.3f" % text_avg, sep='', end='%\t')
+		print("Global: %4.3f" % global_avg, sep='', end='%\t')
+		print("Difference: %4.3f" % difference, sep='', end='%')
+		if not (p) % mod: print('\n')
+		p += 1
 		k += 1
 #--------------------------------------------------------------------------------#
 def Print_Wiki() :
@@ -62,12 +83,10 @@ def Throw_Fatal(error_text) :
 arg_num = len(sys.argv)
 if arg_num != 2 : Throw_Fatal("Invalid arguments.")
 file = sys.argv[1]
-if os.path.isfile(file) == False or file.endswith(".txt") == False :
+if not os.path.isfile(file) or not file.endswith(".txt"):
 	Throw_Fatal("Invalid file.")
 
 # File Processing
-Read_File(file)
+Read_Wiki_File()
+Read_Arg_File(file)
 Print_Result()
-
-#Print_Wiki()
-#print("Total Letter Count:", total_letters)
