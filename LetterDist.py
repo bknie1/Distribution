7be45d4dfc5ffaa719f5491eb7bd3c1bb835882e
bknie1 <brandonknieriem26@gmail.com>
2017-03-21 00:37:36 -0400

    Dictionary implementation.

--- LetterDist.py
+++ LetterDist.py
@@ -1,11 +1,14 @@
 # Name: 		Assignment 3 : Letter Distribution
 # Description : Counts letter occurrences in argument input text file.
 # Author:		Brandon Knieriem
-# Goals:
-# 	- Import multiple books via args. Read and ignore non-alpha.
-# 	- Generate a report for each book. Store in dictionary.
-# 	- Add the sum of all differences to get a total error.
-# 	- Rank books by total error.
+# Notes:
+# 	- Russian: Insignificant deviation. The least amount of deviation.
+# 	- English: Little deviation.
+# 	- Spanish: Very little deviation.
+# 	- French: Little deviation.
+# 	- American: Very little deviation.
+# 	- Irish: Very little deviation.
+# 	Twain and Hugo tied for the highest deviation. (0.021%)
 
 #LIB##############################################################################
 
@@ -16,8 +19,9 @@ import curses.ascii
 #DECLARATIONS####################################################################
 
 wiki_file = "official.txt" # For easy file swapping.
+wiki = {}
 stats = []
-books = {}
+book_shelf = {}
 total_error = 0
 
 #FX###############################################################################
@@ -28,7 +32,7 @@ def Read_Wiki_File() :
 			stats.append(line.rstrip())
 #--------------------------------------------------------------------------------#
 def Process_Book(file) :
-	global books # Dictionary for each book dictionary by name.
+	global book_shelf # Dictionary for each book dictionary by name.
 	book = {} # Dictionary containing letters, occurrences.
 	letters = []
 	occurrences = []
@@ -38,13 +42,13 @@ def Process_Book(file) :
 	total_letters, occurrences = Tally_Value(letters)
 	book['Letters'] = total_letters
 	book['Occurrences'] = occurrences
-	print(file)
-	books[file] = book # Add new entry
+	book_error = Calculate_Book(total_letters, occurrences)
+	book['Error'] = book_error
 
 	#Print_Dictionary(book)
-	#Print_Dictionary(books)
+	#Print_Dictionary(book_shelf)
 
-	Print_Book_Result(total_letters, occurrences)
+	book_shelf[file] = book # Add new entry
 #--------------------------------------------------------------------------------#
 # Iterates through the book, filtering white space, and tallies each ASCII val.
 def Read_Arg_File(file_name) :
@@ -61,6 +65,7 @@ def Read_Arg_File(file_name) :
 # Add increment array slot via ASCII index (left adjusted).
 # Ex. a = 97. array[0] = a's ascii value - 97 constant.
 def Tally_Value(letters) :
+	dict_letter = {}
 	total_letters = 0
 	occurrences = [0] * 26
 	for i in letters :
@@ -72,40 +77,38 @@ def Tally_Value(letters) :
 			Throw_Fatal(error)
 	return total_letters, occurrences
 #--------------------------------------------------------------------------------#
-def Print_Book_Result(total_letters, occurrences) :
+def Calculate_Book(total_letters, occurrences) :
 	global total_error
 	global stats
 	book_error = 0
 	k = 97
-	p = 1
-	mod = 1 # Adjust for column formatting.
 	for i in occurrences :
 		letter = chr(k)
 		text_avg = round((i/total_letters * 100), 2)
 		global_avg = float(stats[k - 97])
 		deviation = text_avg - global_avg
 		book_error += deviation
+
 		print(letter, sep='', end=' = ')
 		print("%10d" % i, sep='', end='\t')
 		print("Average - Text: %4.3f" % text_avg, sep='', end='%\t')
 		print("Global: %4.3f" % global_avg, sep='', end='%\t')
-		print("Deviation: %4.3f" % deviation, sep='', end='%')
-		if not (p) % mod: print('\n')
-		p += 1
+		print("Deviation: %4.3f" % deviation, sep='', end='%\n')
+
 		k += 1
 	print("Book Letter Count:", total_letters, end='\n')
 	print("Book Deviation: %.3f" % book_error, end='%\n')
 	print("\n-------------------------------------------------------------------------")
 	total_error += book_error
-#--------------------------------------------------------------------------------#
-def Print_Book_Results(text_avg, global_avg) :
-	return
-
+	return book_error
 #--------------------------------------------------------------------------------#
 def Print_Dictionary(dictionary) :
 	#print(dictionary.keys())
 	#print(dictionary.values())
-	print(dictionary.items())
+	#print(dictionary.items())
+	for k, v in dictionary.items():
+		print (k, ':', v)
+		print("-------------------------------------------------------------------------")
 #--------------------------------------------------------------------------------#
 def Print_Total_Result() :
 	global total_error
@@ -118,6 +121,7 @@ def Throw_Fatal(error_text) :
 	sys.exit(1)
 
 #MAIN#############################################################################
+book_shelf = {}
 try :
 	Read_Wiki_File()
 except :
@@ -129,3 +133,4 @@ for arg in sys.argv :
 		try : Process_Book(arg)
 		except : Throw_Fatal("Argument file.")
 Print_Total_Result()
+Print_Dictionary(book_shelf)
